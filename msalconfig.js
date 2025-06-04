58349546fbaa98b3156767ca177950592cd139b6
nelsonlauhy <nelson.lau.hy@outlook.com>
2025-06-04 14:31:29 -0400

    auth update

--- msalconfig.js
+++ msalconfig.js
@@ -1,6 +1,6 @@
 const msalConfig = {
   auth: {
-    clientId: "cf01424e-21e3-4b98-bb96-93d91e32cf7b",  // ← from your screenshot
+    clientId: "17f20b04-4113-4a8a-baaf-5dc56b284dac",  // ← from your screenshot
     authority: "https://login.microsoftonline.com/9f6c51b3-4040-49cf-a898-30bb9f2cfc92", // ← your Directory (tenant) ID
     redirectUri: window.location.origin
   },
